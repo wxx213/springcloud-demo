61195d83865219009f6c3220fb15111dde1274ba
Xingxing Wang <stellarwxx@163.com>
2020-07-02 22:21:03 +0800

    lesson7: add feign client codes

---
+++ lesson7/springcloud/feign/src/main/java/com/lynn/feign/ApplicationService.java
@@ -0,0 +1,23 @@
+package com.lynn.feign;
+
+import com.lynn.feign.ApiService;
+import com.lynn.feign.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationService implements ApplicationRunner {
+	@Autowired
+    private ApiService apiService;
+
+	@Override
+    public void run(ApplicationArguments args) throws Exception {
+		try {
+            System.out.println("This is in ApplicationRunner : " + apiService.index());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+	}
+}
