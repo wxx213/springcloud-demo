d63d0035a59a86610acc9da014d543e8b1ffed64
wxx123 <stellarwxx@163.com>
2020-10-13 00:44:49 +0800

    hystrix: add to simulate one of node service not available to test fuse

--- hystrix/springcloud/eurekaclient2/src/main/java/com/lynn/HelloController.java
+++ hystrix/springcloud/eurekaclient2/src/main/java/com/lynn/HelloController.java
@@ -3,6 +3,7 @@ package com.lynn;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.lang.Thread;
 
 @RestController
 public class HelloController {
@@ -12,6 +13,12 @@ public class HelloController {
 
     @RequestMapping("index")
     public String index(){
+	try {
+		Thread.sleep(1000000);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+
         return "Hello World!,端口："+port;
     }
 }
